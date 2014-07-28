f9730f9967e4a1e711f67a5ed82e3846cf5afef3
Curtis Larson <quackware@gmail.com>
2014-07-27 20:38:55 -0400

    Added license

--- src/random-hour.js
+++ src/random-hour.js
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Curtis Larson (QuackWare).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+
 var exec   = require("child_process").exec;
 var prompt = require("prompt");
 var fs     = require("fs");
